9b598c6af210eecf147c74f3ca77e31a867940fa
Rifaut Alexis <ralexis149@gmail.com>
2026-03-15 21:55:49 +0100

    init snmp simulator .NET

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using SnmpSimulator.Commands;
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+app.Configure(config =>
+{
+    config.AddCommand<GreetCommand>("greet")
+        .WithDescription("Greet someone with a message.");
+});
+return app.Run(args);
